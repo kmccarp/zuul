2b42a8b1dd7b13b040a741c5ce2f8aa07b25e22b
Kevin Carpenter™️ <kevin@moderne.io>
2023-10-02 19:36:22 +0000

    refactor: Common static analysis issues
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.staticanalysis.CommonStaticAnalysis?organizationId=RGVmYXVsdA%3D%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- zuul-core/src/main/java/com/netflix/zuul/netty/connectionpool/PooledConnection.java
+++ zuul-core/src/main/java/com/netflix/zuul/netty/connectionpool/PooledConnection.java
@@ -64,11 +64,11 @@ public class PooledConnection {
     }
 
     private ConnectionState connectionState;
-    private long usageCount = 0;
+    private long usageCount;
     private long reqStartTime;
-    private boolean inPool = false;
-    private boolean shouldClose = false;
-    protected boolean released = false;
+    private boolean inPool;
+    private boolean shouldClose;
+    protected boolean released;
 
     public PooledConnection(
             final Channel channel,
@@ -141,7 +141,7 @@ public class PooledConnection {
     }
 
     public boolean isActive() {
-        return (channel.isActive() && channel.isRegistered());
+        return channel.isActive() && channel.isRegistered();
     }
 
     public boolean isInPool() {
